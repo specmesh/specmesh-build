3a321269fee9995271fd513a17e0613b61bee754
neilavery <neil.avery@gmail.com>
2022-12-17 15:27:23 +0000

    consolidate client demo stuff

--- kafka/build.gradle.kts
+++ kafka/build.gradle.kts
@@ -15,12 +15,16 @@ dependencies {
 
     implementation(project(":parser"))
 
-    api("io.confluent:kafka-schema-registry-client:${confluentVersion}")
-    api("io.confluent:kafka-json-schema-provider:${confluentVersion}")
-    api("io.confluent:kafka-avro-serializer:${confluentVersion}")
-    api("io.confluent:kafka-json-schema-serializer:${confluentVersion}")
+    api("io.confluent:kafka-schema-registry-client:$confluentVersion")
+    api("io.confluent:kafka-json-schema-provider:$confluentVersion")
+    api("io.confluent:kafka-avro-serializer:$confluentVersion")
+    api("io.confluent:kafka-json-schema-serializer:$confluentVersion")
     api("com.google.protobuf:protobuf-java:3.21.11")
+    api("io.confluent:kafka-protobuf-provider:$confluentVersion")
     api("io.confluent:kafka-protobuf-serializer:$confluentVersion")
+    api("io.confluent:kafka-streams-protobuf-serde:$confluentVersion")
+    api("io.confluent:kafka-streams-avro-serde:$confluentVersion")
+
 
     api("org.hamcrest:hamcrest-all:$hamcrestVersion")
 
@@ -29,8 +33,11 @@ dependencies {
     implementation("com.google.guava:guava:28.0-jre")
     implementation("com.microsoft.terraform:terraform-client:1.0.0")
 
-    implementation("org.apache.kafka:kafka-clients:${kafkaVersion}")
+
+    implementation("org.apache.kafka:kafka-streams:$kafkaVersion")
+    implementation("org.apache.kafka:kafka-clients:$kafkaVersion")
     implementation("commons-io:commons-io:2.11.0")
+
     testImplementation("org.projectlombok:lombok:1.18.22")
 
 
@@ -42,12 +49,11 @@ dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.4")
     implementation("com.github.spotbugs:spotbugs-annotations:$spotBugsVersion")
 
+    implementation("org.testcontainers:testcontainers:$testcontainersVersion")
+    testImplementation("org.testcontainers:junit-jupiter:$testcontainersVersion")
+    implementation("org.testcontainers:kafka:$testcontainersVersion")
 
-    implementation("org.testcontainers:testcontainers:${testcontainersVersion}")
-    testImplementation("org.testcontainers:junit-jupiter:${testcontainersVersion}")
-    implementation("org.testcontainers:kafka:${testcontainersVersion}")
-
-    testImplementation("org.apache.logging.log4j:log4j-api:${log4jVersion}")
-    testImplementation("org.apache.logging.log4j:log4j-core:${log4jVersion}")
-    testImplementation("org.apache.logging.log4j:log4j-slf4j18-impl:${log4jVersion}")
+    testImplementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
+    testImplementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
+    testImplementation("org.apache.logging.log4j:log4j-slf4j18-impl:$log4jVersion")
 }
