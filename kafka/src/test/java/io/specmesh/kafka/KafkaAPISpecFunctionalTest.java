d5f2119f18409cc6d15f9b898e4b351f3ed09333
neilavery <neil.avery@gmail.com>
2023-01-25 15:43:45 +0000

    unused import

--- kafka/src/test/java/io/specmesh/kafka/KafkaAPISpecFunctionalTest.java
+++ kafka/src/test/java/io/specmesh/kafka/KafkaAPISpecFunctionalTest.java
@@ -31,7 +31,6 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
