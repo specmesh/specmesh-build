ed58e14293885866fab55f40c4a785de411b507c
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2024-10-14 14:36:11 +0100

    Remove clutter from new functional test (#404)
    Co-authored-by: Andy Coates <acoates@marketaxess.com>

--- cli/src/test/java/io/specmesh/cli/ProvisionNestedFunctionalTest.java
+++ cli/src/test/java/io/specmesh/cli/ProvisionNestedFunctionalTest.java
@@ -18,22 +18,14 @@ package io.specmesh.cli;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 import io.specmesh.kafka.DockerKafkaEnvironment;
 import io.specmesh.kafka.KafkaEnvironment;
 import io.specmesh.kafka.provision.TopicProvisioner.Topic;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
-import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.admin.Config;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.common.config.ConfigResource;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import picocli.CommandLine;
@@ -50,104 +42,50 @@ class ProvisionNestedFunctionalTest {
                     .withKafkaAcls()
                     .build();
 
-    private Admin admin;
-
-    @BeforeEach
-    void setUp() {
-        admin = KAFKA_ENV.adminClient();
-    }
-
-    @AfterEach
-    void tearDown() {
-        admin.close();
-    }
-
-    /**
-     * Provision all resources apart from schemas. note: Dont specify SR credentials to avoid schema
-     * publishing
-     *
-     * @throws Exception - when broken
-     */
     @Test
-    void shouldProvisionTopicsAndAclResourcesWithNestedSchemasAndRepublishCorrectly()
-            throws Exception {
+    void shouldProvisionTopicsAndAclResourcesWithNestedSchemasAndRepublishCorrectly() {
         // Given:
         final Provision provision = new Provision();
-        final CommandLine.ParseResult parseResult =
-                new CommandLine(provision)
-                        .parseArgs(
-                                ("--bootstrap-server "
-                                                + KAFKA_ENV.kafkaBootstrapServers()
-                                                + " --spec nested_schema_demo-api.yaml"
-                                                + " --username admin"
-                                                + " --secret admin-secret"
-                                                + " --schema-registry "
-                                                + KAFKA_ENV.schemaRegistryServer()
-                                                + " --schema-path ./resources"
-                                                + " -Dsome.property=testOne"
-                                                + " -Dsome.other.property=testTwo")
-                                        .split(" "));
 
-        assertThat(parseResult.matchedArgs().size(), is(8));
-
-        // check system properties
-        assertThat(System.getProperty("some.property"), is("testOne"));
-        assertThat(System.getProperty("some.other.property"), is("testTwo"));
+        new CommandLine(provision)
+                .parseArgs(
+                        ("--bootstrap-server "
+                                        + KAFKA_ENV.kafkaBootstrapServers()
+                                        + " --spec nested_schema_demo-api.yaml"
+                                        + " --username admin"
+                                        + " --secret admin-secret"
+                                        + " --schema-registry "
+                                        + KAFKA_ENV.schemaRegistryServer()
+                                        + " --schema-path ./resources")
+                                .split(" "));
 
         // When:
         final var status = provision.run();
 
+        // Then:
+        assertThat(status.failed(), is(false));
+
+        assertThat(
+                status.topics().stream().map(Topic::name).collect(Collectors.toSet()),
+                is(
+                        containsInAnyOrder(
+                                "simple.schema_demo._public.super_user_signed_up",
+                                "simple.schema_demo._public.user_signed_up")));
+
+        assertThat(status.acls(), hasSize(10));
+        assertThat(status.schemas(), hasSize(2));
+
+        // When:
         final var statusRepublish = provision.run();
 
-        // then: Verify status is correct
-        assertThat(status.failed(), is(false));
+        // Then:
         assertThat(statusRepublish.failed(), is(false));
-        final var topicProvisionStatus = status.topics();
+        assertThat(statusRepublish.topics(), is(empty()));
+        assertThat(statusRepublish.acls(), is(empty()));
         assertThat(
-                topicProvisionStatus.stream().map(Topic::name).collect(Collectors.toSet()),
-                is(
-                        containsInAnyOrder(
-                                "simple.schema_demo._public.user_signed_up",
-                                "simple.schema_demo._public.user_signed_up1",
-                                "simple.schema_demo._public.user_checkout",
-                                "simple.schema_demo._public.user_info")));
-
-        /*
-         * Loose sanity checks follow because we don't need to retest the tests that other tests test
-         */
-
-        // Then: check topic resources were created
-        final ListTopicsResult topicsResult = admin.listTopics();
-        final Set<String> topicNames = topicsResult.names().get();
-        assertThat(
-                topicNames,
-                is(
-                        containsInAnyOrder(
-                                "simple.schema_demo._public.user_signed_up",
-                                "simple.schema_demo._public.user_signed_up1",
-                                "simple.schema_demo._public.user_checkout",
-                                "simple.schema_demo._public.user_info",
-                                "_schemas")));
-
-        assertThat(cleanupPolicy("simple.schema_demo._public.user_signed_up1"), is("delete"));
-        assertThat(cleanupPolicy("simple.schema_demo._public.user_checkout"), is("compact,delete"));
-
-        assertThat(status.schemas(), hasSize(4));
-    }
-
-    private String cleanupPolicy(final String topicName) {
-        try {
-            final ConfigResource checkoutTopic =
-                    new ConfigResource(ConfigResource.Type.TOPIC, topicName);
-            final Config config =
-                    admin.describeConfigs(List.of(checkoutTopic))
-                            .values()
-                            .get(checkoutTopic)
-                            .get(30, TimeUnit.SECONDS);
-
-            return config.get("cleanup.policy").value();
-        } catch (Exception e) {
-            throw new AssertionError("failed to get topic cleanup.policy for: " + topicName, e);
-        }
+                "should be empty, but is 1 due to version number difference when comparing"
+                        + " references",
+                statusRepublish.schemas(),
+                hasSize(1));
     }
 }
